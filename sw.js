f72f7c41024c6a75720ad9b18fe043f9d66225ee
Ludmilajohnston <ludmilavitoria512@gmail.com>
2026-07-13 08:30:56 -0300

    Upgrade service worker to version 2
    Updated service worker to version 2 with improved caching strategy and added activation event.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "controle-v1";
+const CACHE_NAME = "controle-v2";
 
 const arquivos = [
   "./",
@@ -8,24 +8,37 @@ const arquivos = [
   "./manifest.json",
   "./logo.png",
   "./assinatura.png",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/icon-192.jpg",
+  "./icons/icon-512.jpg"
 ];
 
 self.addEventListener("install", e => {
+  self.skipWaiting(); // ativa a nova versão assim que instalar, sem esperar o usuário fechar todas as abas
   e.waitUntil(
     caches.open(CACHE_NAME)
-    .then(cache => {
-      return cache.addAll(arquivos);
-    })
+    .then(cache => cache.addAll(arquivos))
   );
 });
 
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+    .then(nomes => Promise.all(
+      nomes.filter(nome => nome !== CACHE_NAME).map(nome => caches.delete(nome))
+    ))
+    .then(() => self.clients.claim()) // toma controle das abas já abertas
+  );
+});
+
+// ESTRATEGIA: sempre tenta a rede primeiro (pega a versão mais nova),
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request)
-    .then(response => {
-      return response || fetch(e.request);
-    })
+    fetch(e.request)
+      .then(resposta => {
+        var copia = resposta.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, copia));
+        return resposta;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
